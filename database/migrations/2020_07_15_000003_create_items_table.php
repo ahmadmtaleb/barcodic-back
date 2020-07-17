814a16fb29b13711eea406eaf6f7e72e6fe7e752
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-07-17 12:49:09 +0300

    migration

---
+++ database/migrations/2020_07_15_000003_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('english_name');
+            $table->string('arabic_name');
+            $table->string('barcode')->unique();
+            $table->string('price');
+            $table->string('brand');
+            $table->foreignId('category_id')
+                    ->constrained('categories')
+                    ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
